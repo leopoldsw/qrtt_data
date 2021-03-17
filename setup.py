4df9c22ec27beb03e80e1b686ddfdc86b70a3f87
Leopold W <leopold@lwco.com>
2021-03-17 16:53:14 -0400

    Make structural changes
    Separate out  functions that download stock data into a single file called 'stock_data.py'

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 
-setup(name='stock_data',
+setup(name='qrtt_data',
       version='0.0.3',
-      description='Datasets for stocks.',
+      description='Datasets for stocks and other financial instruments.',
       url='https://data.qrtt.org',
+      download_url = '',
       author='Leopold W.',
       author_email='lsw@lwco.com',
       packages=find_packages(), #exclude=("tests", "tests_dev")
